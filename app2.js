8a451305776678eb5c00d2b4230020c7de7a9b66
Hardik Parmar <hardik.parmar5@gmail.com>
2022-04-04 16:45:39 +0530

    practising updte and find

---
+++ app2.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/hardik");
+  const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+  });
+  const User = mongoose.model("User", userSchema);
+  const user = await User.create({ name: "hardik", age: 29 });
+
+  const filter = { name: "hardik" };
+  const update = { age: 30 };
+  const doc = await User.findOneAndUpdate(filter, update, {
+    new: true,
+  });
+  console.log(user);
+  await doc.save();
+  console.log(doc);
+}
--- user.js
+++ user.js
@@ -7,20 +7,28 @@ async function main() {
   const userSchema = new mongoose.Schema({
     name: String,
     age: Number,
-    email: {
-      //when you field to be required or want it to be lowercase
-      type: String,
-      required: true,
-      lowercase: true,
-    },
+    // email: {
+    //   //when you field to be required or want it to be lowercase
+    //   type: String,
+    //   required: true,
+    //   lowercase: true,
+    // },
   });
   const User = mongoose.model("User", userSchema);
   const user = new User({ name: "hardik", age: 28 });
   const user2 = await User.create({ name: "tikul", age: 29 }); //another way of adding user with saving
-  user.name = "HARDIK"; //for updating
+  // user.name = "HARDIK"; //for updating
   user2.name = "Tikul";
+  // User.email = "hardik@gmail.com";
   await user2.save(); //we have to save this time because of update.
   await user.save();
   console.log(user);
   console.log(user2);
+
+  const filter = { name: "hardik" };
+  const update = { age: 30 };
+  let doc = await User.findOneAndUpdate(filter, update, {
+    new: true,
+  });
+  console.log(doc);
 }
